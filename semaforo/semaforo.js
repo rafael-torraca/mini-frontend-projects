5952e3c8afe06da1505d2eacbe1e0387d94a46e6
Rafael Torraca <rafael.torraca@icloud.com>
2021-08-12 08:48:39 -0300

    semaforo

---
+++ semaforo/semaforo.js
@@ -0,0 +1,41 @@
+const img = document.querySelector("#img");
+const buttons = document.querySelector("#buttons");
+let colorIndex = 0;
+let intervalId = null;
+
+const trafficLight = (event) => {
+  stopAutomatic();
+  turnOn[event.target.id]();
+}
+
+const nextIndex = () => {
+  colorIndex = colorIndex < 2 ? ++colorIndex : 0;
+  
+  // if (colorIndex < 2) {
+  //   colorIndex++;
+  // } else {
+  //   colorIndex = 0;
+  // }
+}
+
+
+
+const changeColor = () => {
+  const colors = ['red', 'yellow', 'green'];
+  const color = colors[colorIndex];
+  turnOn[color]();
+  nextIndex();
+}
+
+const stopAutomatic = () => {
+  clearInterval(intervalId);
+}
+
+const turnOn = {
+  'red': () => img.src = './img/vermelho.png',
+  'yellow': () => img.src = './img/amarelo.png',
+  'green': () => img.src = './img/verde.png',
+  'automatic': () => intervalId = setInterval(changeColor, 1000)
+}
+
+buttons.addEventListener('click', trafficLight);
